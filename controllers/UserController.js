240ca8c204e05d3fb6be3a76ec84de428d578f98
Jose Signoret <jsignoretfernandez@gmail.com>
2021-03-15 23:43:56 +0100

    Refactor endPoint DeleteUser

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -108,7 +108,6 @@ exports.updateUser = async (req, res, next) => {
 
 exports.deleteUser = async (req, res, next) => {
   const userDelete = await Users.findById(req.params.id);
-  console.log(userDelete);
   try {
     /* Comprobamos que exista el usuario */
     if (!userDelete) {
@@ -125,7 +124,7 @@ exports.deleteUser = async (req, res, next) => {
       console.log(prueba1);
 
       /* Borra el usuario y la foto del avatar*/
-      res.json({ msg: "Usuario Borrado Correctamente", del: userDelete });
+      res.json({ msg: "Usuario Borrado Correctamente"});
       //fs.unlinkSync(`./public/images/avatar/${userDelete.avatar}`);
       return;
     }
